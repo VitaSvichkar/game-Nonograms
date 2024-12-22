5e41a0ed514e62aa4e71b691a293a94777b53b28
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-12-22 22:09:38 +0200

    refactor: modify the function to display hints

--- nonograms/scripts/showHints.js
+++ nonograms/scripts/showHints.js
@@ -1,4 +1,6 @@
 function renderHints(arrayHints, obj) {
+  const arrWraps = [];
+
   for (let i = 0; i < obj.cellWrapInRow; i += 1) {
     const wrap = document.createElement('div');
 
@@ -15,16 +17,27 @@ function renderHints(arrayHints, obj) {
       wrap.append(hints);
     }
 
-    return wrap;
+    arrWraps.push(wrap);
   }
+
+  return arrWraps;
 }
 
-export function showHints(obj) {
-  const wrapHintLeft = renderHints(obj.curGame.hintLeft, obj);
-  wrapHintLeft.classList.add('wrap-hint-left');
+export function showHints() {
+  const hintLeft = document.querySelector('.hint-left');
+  const hintTop = document.querySelector('.hint-top');
 
-  const wrapHintTop = renderHints(obj.curGame.hintTop, obj);
-  wrapHintTop.classList.add('wrap-hint-top');
+  return function (obj) {
+    hintLeft.innerHTML = '';
+    hintTop.innerHTML = '';
 
-  return [wrapHintLeft, wrapHintTop];
+    const wrapHintLeft = renderHints(obj.curGame.hintLeft, obj);
+    wrapHintLeft.forEach((el) => el.classList.add('wrap-hint-left'));
+
+    const wrapHintTop = renderHints(obj.curGame.hintTop, obj);
+    wrapHintTop.forEach((el) => el.classList.add('wrap-hint-top'));
+
+    wrapHintLeft.forEach((el) => hintLeft.append(el));
+    wrapHintTop.forEach((el) => hintTop.append(el));
+  };
 }
