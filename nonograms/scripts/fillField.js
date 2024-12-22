67e8dbae649c98343462e707c18a73f665dd1b60
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-12-22 22:11:19 +0200

    refactor: modify the function to fill the field with cells

--- nonograms/scripts/fillField.js
+++ nonograms/scripts/fillField.js
@@ -1,13 +1,21 @@
-export function fillField(obj) {
-  for (let i = 0; i < obj.cellWrapInRow * obj.cellWrapInRow; i += 1) {
-    const cellWrap = document.createElement('div');
-    cellWrap.classList.add('cell-wrap');
+export function fillField() {
+  const gameBlock = document.querySelector('.game');
 
-    for (let j = 0; j < obj.cellsInRow * obj.cellsInRow; j += 1) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      cellWrap.append(cell);
+  return function (obj) {
+    gameBlock.innerHTML = '';
+    const sumCell = obj.cellWrapInRow * obj.cellWrapInRow;
+
+    for (let i = 0; i < sumCell; i += 1) {
+      const cellWrap = document.createElement('div');
+      cellWrap.classList.add('cell-wrap');
+
+      for (let j = 0; j < obj.cellsInRow * obj.cellsInRow; j += 1) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cellWrap.append(cell);
+      }
+
+      gameBlock.append(cellWrap);
     }
-    return cellWrap;
-  }
+  };
 }
