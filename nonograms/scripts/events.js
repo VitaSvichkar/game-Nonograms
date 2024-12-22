c55ee89060edf186535712bb96317715d5b2ddd9
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-12-22 22:37:19 +0200

    feat: create events for select

---
+++ nonograms/scripts/events.js
@@ -0,0 +1,26 @@
+import { updateSelect } from './updateSelect.js';
+import { updateGame } from './updateGame.js';
+
+export function events(obj) {
+  const selectSizes = document.querySelector('.select-sizes');
+  const selectNames = document.querySelector('.select-names');
+
+  selectNames.addEventListener('change', () => {
+    obj.curName = selectNames.value;
+    updateGame(obj);
+  });
+
+  selectSizes.addEventListener('change', () => {
+    obj.curSize = selectSizes.value;
+    obj.cellWrapInRow = selectSizes.value / obj.cellsInRow;
+
+    const gameBlock = document.querySelector('.game');
+    gameBlock.style.setProperty('--cellWrapCount', obj.cellWrapInRow);
+    const selectNames = document.querySelector('.select-names');
+    const keyNames = `valuesGameNames${obj.curSize}`;
+    const arrNames = obj[keyNames];
+    obj.curName = arrNames[0];
+    updateSelect(arrNames, selectNames);
+    updateGame(obj);
+  });
+}
