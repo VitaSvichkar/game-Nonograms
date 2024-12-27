e02ac1ef72df38ccedd6e5b22122af4e80ed5d3e
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-12-28 00:12:00 +0200

    refactor: remove unnecessary code

--- nonograms/scripts/updateGameState.js
+++ nonograms/scripts/updateGameState.js
@@ -1,10 +1,7 @@
-import { checkResultGame } from './checkResultGame.js';
-
 export function updateGameState(obj, ind) {
   if (obj.gameState[ind] === 0) {
     obj.gameState[ind] = 1;
   } else {
     obj.gameState[ind] = 0;
   }
-  checkResultGame(obj);
 }
